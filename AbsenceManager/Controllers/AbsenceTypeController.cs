c3bf563f95edb02b24a73b08d194bc62d7f72466
osaelh <oussamaelh@gmail.com>
2021-02-26 18:21:46 +0100

    Setting up User Access with role authorization

--- AbsenceManager/Controllers/AbsenceTypeController.cs
+++ AbsenceManager/Controllers/AbsenceTypeController.cs
@@ -2,6 +2,7 @@
 using AbsenceManager.Data;
 using AbsenceManager.Models;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,6 +12,7 @@ using System.Threading.Tasks;
 
 namespace AbsenceManager.Controllers
 {
+    [Authorize(Roles ="Administrator")]
     public class AbsenceTypeController : Controller
     {
         private readonly IAbsenceTypeRepository _repo;
