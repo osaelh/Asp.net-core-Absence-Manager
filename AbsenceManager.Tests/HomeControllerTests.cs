160c040c4f66557ad28767a81b1df604baaa5ab3
osaelh <oussamaelh@gmail.com>
2021-02-28 23:11:51 +0100

    Adding The XUnit Project(AbsenceManager.Tests) and setting up the home controller tests(Index action, Privacy action)

---
+++ AbsenceManager.Tests/HomeControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using AbsenceManager.Controllers;
+using Castle.Core.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AbsenceManager.Tests
+{
+   public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_ReturnsViewResultWithNoModel()
+        {
+            //Arrange
+            var mockLogger = new Mock<ILogger<HomeController>>();
+            var controller = new HomeController(mockLogger.Object);
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult);
+            Assert.NotNull(viewResult.ViewData);
+            Assert.Null(viewResult.Model);
+
+        }
+        [Fact]
+        public void Privacy_ReturnsViewResultWithNoModel()
+        {
+            // Arrange
+            var mockLogger = new Mock<ILogger<HomeController>>();
+            var controller = new HomeController(mockLogger.Object);
+
+            // Act
+            var result = controller.Privacy();
+
+            // Assert correct non-null View returned with no Model
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult);
+            Assert.Equal(nameof(controller.Privacy), viewResult.ViewName);
+            Assert.NotNull(viewResult.ViewData);
+            Assert.Null(viewResult.ViewData.Model);
+        }
+    }
+}
